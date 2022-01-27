80fb6470b0e3d686c95dc0493e50c5cd9cc1a132
shinsw627 <shinsw627@naver.com>
2022-01-27 21:31:02 +0900

    동기 비동기 공부

--- 자바스크립트/연습파일/sync_async.js
+++ 자바스크립트/연습파일/sync_async.js
@@ -1,13 +1,19 @@
+setTimeout(function () {
+  console.log("셋타임아웃", Date.now());
+}, 0);
+setTimeout(function () {
+  console.log("셋타임아웃 2", Date.now());
+}, 2000);
+setTimeout(function () {
+  console.log("셋타임아웃 3", Date.now());
+}, 1000);
+
 console.log("시작", Date.now());
 for (let i = 0; i < 10000000; i++) {
   if (i === 9999999) {
     console.log("for문 끝", Date.now());
   }
 }
-
-setTimeout(function () {
-  console.log("셋타임아웃", Date.now());
-}, 0);
 
 Promise.resolve().then(function () {
   console.log("프로미스", Date.now());
